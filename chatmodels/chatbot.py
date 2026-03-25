09fdbde6701f6e02273cc7c00646031039429e04
Ganpat Suthar <rsutharganpat2004@gmail.com>
2026-03-25 17:54:31 +0530

    Added Temorary Chat History List

--- chatmodels/chatbot.py
+++ chatmodels/chatbot.py
@@ -1,25 +1,31 @@
 from dotenv import load_dotenv
 load_dotenv()
-
+from langchain_core.messages import SystemMessage, AIMessage, HumanMessage
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 
 llm = HuggingFaceEndpoint(
     repo_id="deepseek-ai/DeepSeek-R1",
     temperature=0.7,
-    max_new_tokens=512
-)
+    max_new_tokens=512)
 
 chat_model = ChatHuggingFace(llm=llm)
+
+#massages list banai hai taaki chat history isme store ho ske but it still not that good because memory storage is too high for large chats 
+messages = [
+       SystemMessage(content= "You are funny AI Agent")
+]
 
 print("********* Welcome to My Chatbot *********")
 print("---- Enter 0 if you want to Exit ----")
 
 while True:
     prompt = input("You: ")
-
+    
     if prompt == "0":
         print("Bot: Bye, See You Again")
         break
-
-    response = chat_model.invoke(prompt)
+    messages.append(HumanMessage(content=prompt))
+    response = chat_model.invoke(messages)
+    messages.append(AIMessage(response.content))
     print("Bot:", response.content)
+print(messages)
