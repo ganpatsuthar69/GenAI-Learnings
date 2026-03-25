814be3545b1a8690e219e05ecca7df8016c5fd22
Ganpat Suthar <rsutharganpat2004@gmail.com>
2026-03-25 18:06:45 +0530

    added system messages

--- chatmodels/chatbot.py
+++ chatmodels/chatbot.py
@@ -9,10 +9,24 @@ llm = HuggingFaceEndpoint(
     max_new_tokens=512)
 
 chat_model = ChatHuggingFace(llm=llm)
+print("chosse your AI mode")
+print("press 1 for Angry mode")
+print("press 2 for funny mode ")
+print("press 3 for sad mode")
+
+choice = int(input("tell your response :- "))
+
+if choice == 1:
+    mode = "You are an angry AI agent. You respond aggressively and impatiently."
+elif choice == 2:
+    mode = "You are a very funny AI agent. You respond with humor and jokes."
+elif choice == 3:
+    mode = "You are a very sad AI agent. You respond in a depressed and emotional tone."
+
 
 #massages list banai hai taaki chat history isme store ho ske but it still not that good because memory storage is too high for large chats 
 messages = [
-       SystemMessage(content= "You are funny AI Agent")
+       SystemMessage(content= mode)
 ]
 
 print("********* Welcome to My Chatbot *********")
